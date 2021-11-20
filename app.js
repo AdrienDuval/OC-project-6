2ed53d5bcc396efd930a6eb9510975beacad54db
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2021-11-20 14:51:47 +0530

    javascript to show single image in photographer page

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll(".single-image img");
+const original = document.querySelector(".full-img");
+const caption = document.querySelector(".caption");
 
+previews.forEach((preview) => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open");
+        original.classList.add("open");
+        // dynamic change text and image
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = originalSrc;
+        const altText = preview.alt;
+        caption.textContent = altText;
+    });
+});
+modal.addEventListener('click', (e) => {
+   if (e.target.classList.contains("modal")) {
+       modal.classList.remove("open");
+       original.classList.remove("open");
+   }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var modalBtn = document.getElementById("modal");
+// var closeBtn = document.getElementById("close-btn");
+// var imageContainer = document.getElementById("image-container");
+
+// function enlargeImage () {
+//     modalBtn.classList.add("enlarge-image");
+//     closeBtn.style.display = "block";
+//     imageContainer.classList.remove('image-container-hover');
+// }
+
+// modalBtn.addEventListener("click", enlargeImage);
